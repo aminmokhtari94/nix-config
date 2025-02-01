d2f71488a12f10a0eaadecdf1686acadb6a35ca8
Amin Mokhtart <amin.mokhtari94@gmail.com>
2025-02-01 08:54:29 +0330

    chore: WIP themes

--- modules/home/themes/default.nix
+++ modules/home/themes/default.nix
@@ -5,16 +5,9 @@ let cfg = config.default.theme;
 in {
   options.default.theme = with types; {
     name = mkOption {
-      type = types.str;
+      type = str;
       default = "inspired";
-      description = "Name of theme to apply apps and tools appearance";
+      description = "Name of theme to apply to apps and tools appearance";
     };
   };
-
-  # Dynamically import the theme configuration file
-  #config = mkIf (builtins.pathExists ./${cfg.name}.theme.nix) (
-  #  import ./${cfg.name}.theme.nix {
-  #    inherit pkgs;
-  #  }
-  #);
 }
