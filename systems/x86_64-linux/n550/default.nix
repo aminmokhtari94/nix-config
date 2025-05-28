9456560615a192f172c8e7a4215b405985ba6f28
Amin Mokhtart <amin.mokhtari94@gmail.com>
2025-05-28 15:48:23 +0330

    n550 etc and golang

--- systems/x86_64-linux/n550/default.nix
+++ systems/x86_64-linux/n550/default.nix
@@ -22,6 +22,31 @@
   # Enable networking
   networking.networkmanager.enable = true;
   networking.nameservers = [ "1.1.1.1" "9.9.9.9" ];
+  networking.extraHosts = ''
+
+    # 172.16.100.40 api.kiz.ir emqx.kiz.ir asset.kiz.ir reg.kiz.ir minio.kiz.ir ops.kiz.ir api-v2.kiz.ir akhq.abrso.ir
+    # 172.16.100.40 abrso.ir app.abrso.ir cms.abrso.ir next.abrso.ir api-next.abrso.ir api.abrso.ir emqx.abrso.ir metabase.abrso.ir
+    # 172.16.100.40 terabar.ir app.terabar.ir cms.terabar.ir influxdb.abrso.ir
+    # 172.16.100.40 rahkarsanat.ir cms.rahkarsanat.ir taiga.rahkarsanat.ir git.kiz.ir redpanda.kiz.ir
+    # 172.16.100.41 acl.kiz.ir grpc.abrso.ir grpc.kiz.ir terabar.acl.kiz.ir lone.acl.kiz.ir abrso.acl.kiz.ir grpc.terabar.ir all.kiz.ir
+    # 172.16.100.45 mqtt.abrso.ir
+
+    172.16.100.205 k8s.c02.kiz.ir
+    # 185.177.158.57 k8s.c02.kiz.ir
+    172.16.100.40  grafana.prometheus.cluster.local
+
+    127.0.0.1 mongodb-0.mongodb-headless.kiz.svc.cluster.local
+    127.0.0.1 mongodb-1.mongodb-headless.kiz.svc.cluster.local
+    127.0.0.1 mongodb-2.mongodb-headless.kiz.svc.cluster.local
+    127.0.0.1 mongo-psmdb-db-rs0.kiz-db.svc.cluster.local
+    127.0.0.1 mongo-psmdb-db-rs0-0.mongo-psmdb-db-rs0.kiz-db.svc.cluster.local
+    127.0.0.1 mongo-psmdb-db-rs0-1.mongo-psmdb-db-rs0.kiz-db.svc.cluster.local
+    127.0.0.1 mongo-psmdb-db-rs0-2.mongo-psmdb-db-rs0.kiz-db.svc.cluster.local
+
+    172.16.100.201 redpanda-0
+    172.16.100.209 redpanda-1
+    172.16.100.202 redpanda-2
+  '';
 
   # Set your time zone.
   time.timeZone = "Asia/Tehran";
