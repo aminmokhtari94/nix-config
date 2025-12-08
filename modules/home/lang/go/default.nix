c5614a7fa0ac87768805e96eb29ac237c35727c1
Amin Mokhtart <amin.mokhtari94@gmail.com>
2025-12-08 17:08:09 +0330

    upgrade to 25.11

--- modules/home/lang/go/default.nix
+++ modules/home/lang/go/default.nix
@@ -1,16 +1,33 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 with lib;
-let cfg = config.default.lang.go;
-in {
+let
+  cfg = config.default.lang.go;
+in
+{
   options.default.lang.go = with types; {
     enable = mkEnableOption "Golang language support";
   };
 
   config = mkIf cfg.enable {
-    programs.go = { enable = true; };
-    home.packages = with pkgs; [ gopls gnumake protobuf buf gcc ko ];
+    programs.go = {
+      enable = true;
+    };
+    home.packages = with pkgs; [
+      gopls
+      gnumake
+      protobuf
+      buf
+      gcc
+      ko
+    ];
     home.sessionPath = [ "$HOME/go/bin" ];
     programs.nixvim = {
+      plugins.dap.enable = true;
       plugins.dap-go.enable = true;
       # extraPlugins = [
       #   (pkgs.vimUtils.buildVimPlugin {
